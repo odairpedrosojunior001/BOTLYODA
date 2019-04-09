7df6a3f0171bd227595b3e8fa8c71234d4baeca4
Odair Pedroso <odair.junior@luizalabs.com>
2019-04-09 14:02:52 -0300

    deploy01

---
+++ deploy01.js
@@ -0,0 +1,37 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+ 
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function welcome(agent) {
+    agent.add('Olá, bem vindo ao bot.');            
+  }
+    
+  function fallback(agent) {
+    agent.add('Desculpe não entendi.');    
+  }
+  // Inserindo mais uma função para responder preço de curso:
+  function preco(agent){
+    agent.add('100 ');
+  }
+  
+  // Run the proper function handler based on the matched Dialogflow intent name:
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('preco.curso', preco);
+
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
